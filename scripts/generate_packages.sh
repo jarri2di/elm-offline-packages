7d140778a580269fa32f120fe82b467292c83152
jarri2di <joe.arrington@gmail.com>
2020-01-29 21:14:55 -0500

    chore: updated elm packages

--- scripts/generate_packages.sh
+++ scripts/generate_packages.sh
@@ -9,47 +9,47 @@ elm_dir=~/.elm
 success=1
 
 function cleanup() {
-    if [[ -d "elm-stuff" ]] ; then
-        rm -rf elm-stuff
-    fi
+  if [[ -d "elm-stuff" ]]; then
+    rm -rf elm-stuff
+  fi
 
-    if [[ -d "src" ]] ; then
-        rm -rf src
-    fi
+  if [[ -d "src" ]]; then
+    rm -rf src
+  fi
 
-    if [[ -f "elm.json" ]] ; then
-        rm elm.json
-    fi
+  if [[ -f "elm.json" ]]; then
+    rm elm.json
+  fi
 }
 
 function init() {
-    # Start clean
-    if [[ -d "$elm_dir" ]] ; then
-        echo -e "Deleting pre-existing $elm_dir.\n"
-        rm -rf "$elm_dir"
-    fi
+  # Start clean
+  if [[ -d "$elm_dir" ]]; then
+    echo -e "Deleting pre-existing $elm_dir.\n"
+    rm -rf "$elm_dir"
+  fi
 
-    cleanup
+  cleanup
 
-    echo -e "Now attempting to install packages.\n"
-    sleep 2
+  echo -e "Now attempting to install packages.\n"
+  sleep 2
 }
 
 function install_packages() {
-    # Re-init to avoid any package dependency issues / hangs
-    for package in ${packages}; do
-        success=0
-        yes | elm init
-        yes | elm install "$package"
-        cleanup
-    done
+  # Re-init to avoid any package dependency issues / hangs
+  for package in ${packages}; do
+    success=0
+    yes | elm init
+    yes | elm install "$package"
+    cleanup
+  done
 
-    if [[ ${success} -eq 0 ]] ; then
-        echo -e "Successfully installed elm packages.\n"
-        cleanup
-    else
-        echo -e "Something seems to have gone wrong. No packages were installed.\n"
-    fi
+  if [[ ${success} -eq 0 ]]; then
+    echo -e "Successfully installed elm packages.\n"
+    cleanup
+  else
+    echo -e "Something seems to have gone wrong. No packages were installed.\n"
+  fi
 }
 
 # Main execution
